6fd830034d202ca830bb16d002c28cc3cc14cd59
Samriddhi Jaiswal <66547184+sammy-j-hub@users.noreply.github.com>
2020-09-27 19:21:44 +0530

    Add files via upload

---
+++ build/precache-manifest.faf73fc48f59420a3c88519b45c0ab68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd36b72dddd65c55223d1e3fbc1aae1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47530ce8f1cf39ff7a5b",
+    "url": "/static/css/main.44c7ec63.chunk.css"
+  },
+  {
+    "revision": "08fe7eef069f7c8d6da6",
+    "url": "/static/js/2.6c1be3e7.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.6c1be3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47530ce8f1cf39ff7a5b",
+    "url": "/static/js/main.58ad9bf5.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
